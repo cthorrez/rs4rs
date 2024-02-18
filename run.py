9be57faf5a903ec1b38eb2bf3801d3c06c53391e
cthorrez <claytonthorrez@gmail.com>
2024-02-18 15:11:01 -0800

    add some sweep graph stuff

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import random
+import itertools
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,6 +9,24 @@ from riix.models.glicko import Glicko
 from riix.metrics import binary_metrics_suite
 from riix.eval import evaluate, train_and_evaluate
 
+GLICKO_PARAMS_GRID = {
+    'initial_rating_dev': np.linspace(10,100, num=15),
+    'c': np.linspace(10,100, num=15),
+}
+
+def hyperparameter_sweep(model_class, dataset, params_grid, seed=10):
+    random.seed(seed)
+    keys, values = zip(*params_grid.items())
+    params_combs = [dict(zip(keys, v)) for v in itertools.product(*values)]
+    random.shuffle(params_combs)
+    all_metrics = []
+    for params in params_combs:
+        model = model_class(dataset.competitors, **params)
+        metrics = evaluate(model, dataset)
+        print(f'{params} acc: {metrics["accuracy"]}')
+        all_metrics.append(metrics)
+    return all_metrics
+        
 
 def evaluate_dataset(dataset):
     elo = Elo(dataset.competitors)
@@ -24,15 +44,24 @@ def main():
         competitor_cols=['team_1', 'team_2'],
         datetime_col='date',
         outcome_col='outcome',
-        rating_period='30D'
+        rating_period='7D'
     )
     print('evaluating nba')
-    evaluate_dataset(nba_dataset)
+    # evaluate_dataset(nba_dataset)
 
     chess_dataset = MatchupDataset.load_from_npz('data/chess/processed.npz')
     print('evaluating chess')
-    evaluate_dataset(chess_dataset)
+    # evaluate_dataset(chess_dataset)
 
+    nba_glicko_metrics = hyperparameter_sweep(Glicko, nba_dataset, GLICKO_PARAMS_GRID)
+
+    x = np.arange(len(nba_glicko_metrics))
+    # y = [metric['accuracy'] for metric in nba_glicko_metrics]
+    y = np.maximum.accumulate([metric['accuracy'] for metric in nba_glicko_metrics])
+    plt.plot(x,y)
+    plt.xlabel('iteration')
+    plt.ylabel('accuracy')
+    plt.show()
 
 if __name__ == '__main__':
     main()
