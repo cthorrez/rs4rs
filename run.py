a37a702ad0ef1d56ed18a4313fd2d8fd0d7166fc
cthorrez <claytonthorrez@gmail.com>
2024-02-27 23:24:56 -0800

    mp cleanup

--- run.py
+++ run.py
@@ -12,12 +12,12 @@ from riix.metrics import binary_metrics_suite
 from riix.eval import evaluate, train_and_evaluate
 
 GLICKO_PARAMS_GRID = {
-    'initial_rating_dev': np.linspace(1,100, num=10),
-    'c': np.linspace(10,100, num=10),
+    'initial_rating_dev': np.linspace(1,100, num=20),
+    'c': np.linspace(10,100, num=20),
 }
 
 def hyperparameter_sweep(model_class, dataset, params_grid, seed=10):
-    pool = multiprocessing.Pool(4)
+    pool = multiprocessing.Pool(8)
     random.seed(seed)
     keys, values = zip(*params_grid.items())
     params_combs = [dict(zip(keys, v)) for v in itertools.product(*values)]
@@ -49,15 +49,9 @@ def main():
         outcome_col='outcome',
         rating_period='7D'
     )
-    print('evaluating nba')
-    # evaluate_dataset(nba_dataset)
-
-    chess_dataset = MatchupDataset.load_from_npz('data/chess/processed.npz')
-    print('evaluating chess')
-    # evaluate_dataset(chess_dataset)
+    # chess_dataset = MatchupDataset.load_from_npz('data/chess/processed.npz')
 
     nba_glicko_metrics = hyperparameter_sweep(Glicko, nba_dataset, GLICKO_PARAMS_GRID)
-
     x = np.arange(len(nba_glicko_metrics))
     # y = [metric['accuracy'] for metric in nba_glicko_metrics]
     y = np.maximum.accumulate([metric['accuracy'] for metric in nba_glicko_metrics])
